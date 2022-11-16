4d5a75a8fd934bfedcac1305daff09cb019d316e
Evgeni Hristov <evgeni.hristov@bbraun.com>
2022-11-16 17:14:42 +0200

    Documentation as a code first chapter 5

--- docs/markdown/.vitepress/config.js
+++ docs/markdown/.vitepress/config.js
@@ -1,29 +1,27 @@
 import {withMermaid} from "vitepress-plugin-mermaid";
 
 export default withMermaid({
-  export default {
-    title: 'My custom title',
-    description: 'Just playing around.',
-    base: '/miserv.io/',
-    themeConfig: {
-      sidebar: [
-        {
-          text: 'Guide',
-          items: [
-            {text: 'Introduction', link: '/introduction'},
-            {text: 'Getting Started', link: '/getting-started'}
-          ]
-        }
-      ],
-      footer: {
-        message: 'Released under the MIT License.',
-        copyright: 'Copyright © 2022-present Evgeniy Hristov'
-      },
-      algolia: {
-        appId: '2OOIGY80VU',
-        apiKey: 'fec5b03dbb9ec32368c34d3b660cfda3',
-        indexName: 'miservio-docs'
+  title: 'My custom title',
+  description: 'Just playing around.',
+  base: '/miserv.io/',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          {text: 'Introduction', link: '/introduction'},
+          {text: 'Getting Started', link: '/getting-started'}
+        ]
       }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-present Evgeniy Hristov'
+    },
+    algolia: {
+      appId: '2OOIGY80VU',
+      apiKey: 'fec5b03dbb9ec32368c34d3b660cfda3',
+      indexName: 'miservio-docs'
     }
   }
 });
