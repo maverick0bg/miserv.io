6778839b9a43f567fe639536e5b47150608f3239
maverick0bg <maverick.bg@gmail.com>
2022-11-17 19:06:09 +0200

    Feature/documentation as a code (#1)
    Documentation as code

---
+++ docs/markdown/.vitepress/config.js
@@ -0,0 +1,30 @@
+import {withMermaid} from "vitepress-plugin-mermaid";
+
+export default withMermaid({
+  title: 'My custom title',
+  description: 'Just playing around.',
+  base: '/miserv.io/',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          {text: 'Introduction', link: '/introduction'},
+          {text: 'Getting Started', link: '/getting-started'}
+        ]
+      }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-present Evgeniy Hristov'
+    },
+    algolia: {
+      appId: '2OOIGY80VU',
+      apiKey: 'fec5b03dbb9ec32368c34d3b660cfda3',
+      indexName: 'miservio-docs'
+    }
+  }
+});
+
+
+
